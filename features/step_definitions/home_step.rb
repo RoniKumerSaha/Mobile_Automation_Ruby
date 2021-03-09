71635f8eefc611d11848408d35759583180d9148
rsaha <rsaha@rsglab.com>
2021-03-09 16:28:55 +0600

    added a test with steps

--- features/step_definitions/home_step.rb
+++ features/step_definitions/home_step.rb
@@ -1,5 +1,5 @@
 When(/^I click the menu bar$/) do
-
+  find_element(xpath: "//android.widget.ImageButton[@content-desc='Open navigation drawer']").click
 end
 
 Given(/^I am in the home page$/) do
@@ -7,5 +7,5 @@ Given(/^I am in the home page$/) do
 end
 
 Then(/^I should see Menu items$/) do
-
+  find_element(id: "drawerCategoryName").displayed?
 end
